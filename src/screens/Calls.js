4bc9117ca93e2313262e0ad009ca507fdefc5774
MuhammedNiyad <niyadpunniyath007@gmail.com>
2024-05-01 17:42:20 +0530

    status page completed

--- src/screens/Calls.js
+++ src/screens/Calls.js
@@ -1,10 +1,32 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import DATA from '../data/chatData';
+
+import {
+  Fontisto,
+  MaterialIcons,
+  MaterialCommunityIcons,
+} from "react-native-vector-icons";
 
 const Calls = () => {
+  const [callData, setCallData] = useState(DATA);
+
+  useEffect(() => {
+    setCallData(DATA);
+  },[])
   return (
 	<View style={styles.container}>
-	  <Text>Calls</Text>
+      <TouchableOpacity>
+        <View style={styles.callContainer}>
+          <View style={styles.linkContainer}>
+            <Fontisto name="link" size={22} color="white" />
+          </View>
+          <View>
+            <Text style={styles.callLink}>Create a call link</Text>
+            <Text style={styles.subtext}>Share a link for your whatsApp call</Text>
+          </View>
+        </View>
+    </TouchableOpacity>
 	</View>
   )
 }
@@ -15,7 +37,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
+  callContainer: {
+    flexDirection: "row",
+    alignItems: 'center',
+    padding:16,
+  },
+  linkContainer: {
+    width: 50,
+    height: 50,
+    borderRadius:25,
+    backgroundColor: "#128c7e",
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight:16
+  }
 });
