d4fbfa1b340c1e6fa28f7c2194ca80ceb419f5ad
Muhammed Niyad <niyadpunniyath007@gmail.com>
2024-05-02 07:18:57 +0530

    whatapp clone building completed

--- src/screens/Calls.js
+++ src/screens/Calls.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image } from 'react-native'
 import React, { useEffect, useState } from 'react';
 import DATA from '../data/chatData';
 
@@ -23,9 +23,32 @@ const Calls = () => {
           </View>
           <View>
             <Text style={styles.callLink}>Create a call link</Text>
-            <Text style={styles.subtext}>Share a link for your whatsApp call</Text>
+            <Text style={styles.callSubtext}>Share a link for your whatsApp call</Text>
           </View>
         </View>
+    </TouchableOpacity>
+    <Text style={styles.recentText}>Recent</Text>
+    <FlatList data={callData} keyExtractor={(item)=>item.id.toString()}
+    renderItem={({item})=>(
+      <View style={styles.callContainer}>
+        <Image source={item.photos} style={styles.image}/>
+        <View style={styles.callIconContainer}>
+          <View>
+            <Text style={styles.callName}>{item.name}</Text>
+            <View style={styles.callDetails}>
+              <MaterialCommunityIcons name='call-received' size={15} color="#075e54" />
+              <Text style={styles.callTime}>{item.time}</Text>
+            </View>
+          </View>
+        </View>
+        <TouchableOpacity>
+          <MaterialIcons name="call" size={22} color="#075e54" />
+        </TouchableOpacity>
+      </View>
+    )}/>
+    <TouchableOpacity style={styles.callButton} 
+    onPress={()=>('')}>
+      <MaterialIcons name="add-call" size={22} color="white" />
     </TouchableOpacity>
 	</View>
   )
@@ -51,5 +74,55 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginRight:16
+  },
+  callLink:{
+    fontSize:16,
+    fontWeight:'600'
+  },
+  callSubtext:{
+    fontSize:14,
+    color:'grey'
+  },
+  recentText:{
+    fontSize:15,
+    fontWeight:'600',
+    color:"grey",
+    marginVertical:10,
+    marginLeft:16
+  },
+  image:{
+    width:50,
+    height:50,
+    borderRadius:25,
+    marginRight:16,
+  },
+  callName:{
+    fontSize:16,
+    fontWeight:'600',
+  },
+  callDetails:{
+    flexDirection:'row',
+    alignItems:"center"
+  },
+  callTime:{
+    fontSize:14,
+    color:'grey',
+    marginLeft:'5'
+  },
+  callIconContainer:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+    flex:1
+  },
+  callButton:{
+    position:"absolute",
+    bottom:30,
+    right:20,
+    backgroundColor:'#0e806a',
+    padding:16,
+    borderRadius:30,
+    alignItems:'center',
+    justifyContent:'center'
   }
 });
